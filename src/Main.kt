ef7cbb8ad983ee4cf77e1884c7d34de918eca61b
Andrey <56609893+sn41@users.noreply.github.com>
2024-12-27 10:02:06 +0500

    I2

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.File
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val file = File("resources/tab2.csv")
+
+    file.forEachLine {
+        println(it)
     }
+
+    file.forEachLine { line ->
+        println(line)
+    }
+
+    file.forEachLine() { line ->
+        println(line)
+    }
+
+    file.forEachLine(Charsets.UTF_8) { line ->
+        println(line)
+    }
+
+    file.forEachLine( Charsets.UTF_8, action = { line->
+        println(line)
+    })
+
+    file.forEachLine(Charsets.UTF_8, action = action)
+    file.forEachLine(Charsets.UTF_8, action = ::f)
+}
+
+
+val action: (String) -> Unit = { line ->
+    println(line)
+}
+
+fun f(line:String){
+    println(line)
 }
